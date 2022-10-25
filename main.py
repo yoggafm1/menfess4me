faa1e402a61d07b0ae44e8f8dceddfe163d9ac75
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2022-10-25 12:39:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ Bot = Client(
     bot_token=os.environ.get("BOT_TOKEN", "5699253839:AAG3OLhh8YcuVskn0tkrOrYZZ-Nbon_4Faw"),
     api_id=int(os.environ.get("API_ID", "23693414")),  # type: ignore
     api_hash=os.environ.get("API_HASH", "7886b6a15d0a1a06c7feeaeeb6ad6210"),
-    sleep_threshold=3600,
 )
 
 
@@ -27,12 +26,12 @@ async def start(_, update: Message):
     await update.reply_text(
         f"Hello {update.from_user.mention}, I am a telegram bot module for counting total number of clicks on a button.\n\nMade By @phobiakaliann"
     )
-
+    
+LOG=-1001661793479
 
 @Bot.on_message(filters.command(["confes"]))
 async def confess(client: Client, update: Message):
     user_id = update.chat.id
-    LOG = "-1001661793479"
     nama = await client.ask(user_id, 'Masukan Nama kamu', filters=filters.text)
     tujuan = await client.ask(user_id, 'Kepada siapa yang ingin kamu confess?', filters=filters.text)
     isi = await client.ask(user_id, 'apa yang ingin kamu sampaikan', filters=filters.text)
