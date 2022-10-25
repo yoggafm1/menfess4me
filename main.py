99c5c856e524ec50cf5ac7f6038173afc0ab3c5d
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2022-10-25 15:23:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,15 +35,28 @@ async def confess(client: Client, update: Message):
     nama = await client.ask(user_id, '🗣 <b>Ketik Nama kamu</b>\n\n<b>Informasi :</b> __Pakai nama kamu,Jika ingin privasi nama silakan gunakan `Anonim` saja__', filters=filters.text, timeout=30)
     tujuan = await client.ask(user_id, '🗣 <b>Ketik Nama Crush kamu</b>\n\n<b>Informasi :</b> __Wajib pakai username/nama__', filters=filters.text, timeout=30)
     isi = await client.ask(user_id, f"🗣 <b>Ketik apa yang ingin kamu sampaikan kepada {tujuan.text}</b>", filters=filters.text, timeout=30)
-    report = await client.send_message(LOG, f"<b>From :</b> <i>{nama.text}</i>\n<b>To :</b> <i>{tujuan.text}</i>\n<b>Isi :</b> <i>{isi.text}</i>", disable_web_page_preview=True)
-    await client.send_message(user_id, f"✅ **Sudah terkirim**", 
-                              reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡ View", url=f"https://t.me/fvconfess/{report.id}")]]),
-                              disable_web_page_preview=True,
-                             )
+    if nama == "/confes":
+        await client.send_message(user_id, "Format kamu salah silakan coba lagi")
+    if nama == "/start":
+        await client.send_message(user_id, "Format kamu salah silakan coba lagi")
+    if nama == "/kritik":
+        await client.send_message(user_id, "Format kamu salah silakan coba lagi")
+    if tujuan == "/confes":
+        await client.send_message(user_id, "Format kamu salah silakan coba lagi")
+    if tujuan == "/start":
+        await client.send_message(user_id, "Format kamu salah silakan coba lagi")
+    if tujuan == "/kritik":
+        await client.send_message(user_id, "Format kamu salah silakan coba lagi")
+    else:
+        report = await client.send_message(LOG, f"<b>From :</b> <i>{nama.text}</i>\n<b>To :</b> <i>{tujuan.text}</i>\n<b>Isi :</b> <i>{isi.text}</i>", disable_web_page_preview=True)
+        await client.send_message(user_id, f"✅ **Sudah terkirim**", 
+                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡ View", url=f"https://t.me/fvconfess/{report.id}")]]),
+                                  disable_web_page_preview=True,
+                                 )
     
 KR=-1001847941518
 
-@Bot.on_message(filters.command(["kritik"]))
+@Bot.on_message(filters.command(["kritik"]) & filters.private)
 async def kritik(client: Client, update: Message):
     user_id = update.chat.id
     tujuan = await client.ask(user_id, '🗣 <b>Ketik Nama Admin yang ingin kamu kritik</b>\n\n<b>Informasi :</b> __Wajib pakai username/nama__', filters=filters.text, timeout=30)
